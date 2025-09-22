6db09c5390c50d699c990c7f9e91c79507da3592
Omar Walid Diab <omardiab25213@gmail.com>
2025-09-22 22:52:30 +0300

    Added Pathfinding python module build for Raspberry Pi OS

---
+++ Pathfinding/test.py
@@ -0,0 +1,23 @@
+import os
+import sys
+# Add MinGW bin folder to DLL search path
+# os.add_dll_directory(r"D:\msys2\mingw64\bin")
+import pathfinding
+
+# Define the grid (must be 9x10 because ROW=9, COL=10 in your C++ code)
+grid = [
+    [1, 1, 1, 1, 1, 1, 0, 0, 1, 1],
+    [1, 0, 0, 1, 1, 1, 1, 0, 1, 0],
+    [1, 1, 1, 0, 1, 1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 0, 0, 1, 1],
+    [1, 1, 1, 1, 1, 0, 1, 1, 0, 1],
+    [0, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 0, 1, 0, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 0, 1]
+]
+
+# Call the function
+path = pathfinding.aStarSearch(grid, [0,0], [8,0])
+
+print("Path:", path)
