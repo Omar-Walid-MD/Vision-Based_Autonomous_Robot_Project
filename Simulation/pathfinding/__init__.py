ee80266dba91d07998725b8e12c3e21a6e8af7ba
Omar-Walid-MD <omar.madinah49@yahoo.com>
2025-10-10 20:56:15 +0300

    Deleted copied dlls

--- Simulation/pathfinding/__init__.py
+++ Simulation/pathfinding/__init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 import platform
 
-# system = platform.system()
-# if system == "Windows":
-#     os.add_dll_directory(r"D:\msys2\mingw64\bin")
+system = platform.system()
+if system == "Windows":
+    os.add_dll_directory(r"D:\msys2\mingw64\bin")
 from .pathfinding import aStarSearch
